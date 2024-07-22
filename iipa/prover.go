462e6e7b7955167a2fe233c1d4d726d3ff4f14eb
qbh <1328738411@qq.com>
2024-07-23 02:28:41 +0800

    Update prover.go

--- iipa/prover.go
+++ iipa/prover.go
@@ -4,6 +4,14 @@ type Prover struct {
 	crs *CRS
 }
 
-func (p Prover) RecursiveProve() {
+func (p Prover) RecursiveProve(n int) {
+	if n == 1 {
 
+	}
+	if n%2 == 1 {
+
+	}
+	n1 := n / 2
+	p.RecursiveProve(n1)
+	return
 }
