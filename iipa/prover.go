016ec90812be2e1b36e46c9a86ae1fc019b57d05
qbh <1328738411@qq.com>
2024-07-27 04:40:01 +0800

    Update prover.go

--- iipa/prover.go
+++ iipa/prover.go
@@ -7,26 +7,27 @@ type Prover struct {
 	kyber.Group
 }
 
-// RecursiveProve
-func (p Prover) RecursiveProve(g_vec []kyber.Point, h, P kyber.Point, a_vec, b_vec []kyber.Scalar, u kyber.Scalar, n int) {
-	aLen := len(a_vec)
-	gLen := len(g_vec)
-	if aLen != len(b_vec) {
+func (p Prover) RecursiveProve(gVec []kyber.Point, h, P kyber.Point, aVec, bVec []kyber.Scalar, n int) {
+	aLen := len(aVec)
+	gLen := len(gVec)
+	if aLen != len(bVec) {
 		return
 	}
 	// step 1
 	if n == 1 {
 		return
 	}
-	var proofStep []kyber.Scalar
 
+	var proofStep []kyber.Scalar
 	// step 2
 	if n%2 == 1 {
-		aNeg := p.Group.Scalar().Neg(a_vec[aLen-1])
-		bNeg := p.Group.Scalar().Neg(b_vec[aLen-1])
-		tmp1 := p.Group.Point().Mul(aNeg, g_vec[gLen-1])
-		tmp2 := p.Group.Point().Mul(bNeg, h)
-		proofStep = append(proofStep, na, nb)
+		aNeg := p.Group.Scalar().Neg(aVec[aLen-1])
+		bNeg := p.Group.Scalar().Neg(bVec[aLen-1])
+		tmp1 := p.Group.Point().Mul(aNeg, gVec[gLen-1])
+		tmp2 := p.Group.Point().Mul(p.Group.Scalar().Mul(aVec[aLen-1], bNeg), h)
+
+		P = p.Group.Point().Add(p.Group.Point().Add(tmp1, tmp2), P)
+		proofStep = append(proofStep, aNeg, bNeg)
 		n = n - 1
 	}
 
@@ -36,13 +37,12 @@ func (p Prover) RecursiveProve(g_vec []kyber.Point, h, P kyber.Point, a_vec, b_v
 	cr := p.Group.Scalar().Zero()
 	var L, R kyber.Point
 	for i := 0; i < n1; i++ {
-		tmp1 := p.Group.Scalar().Mul(a_vec[:n1][i], b_vec[n1:][i])
+		tmp1 := p.Group.Scalar().Mul(aVec[:n1][i], bVec[n1:][i])
 		cl = p.Group.Scalar().Add(cl, tmp1)
-		tmp2 := p.Group.Scalar().Mul(a_vec[n1:][i], b_vec[:n1][i])
+		tmp2 := p.Group.Scalar().Mul(aVec[n1:][i], bVec[:n1][i])
 		cr = p.Group.Scalar().Add(cr, tmp2)
-		L = p.Group.Point().Mul(a_vec[:n1][i], g_vec[n1:][i])
-		R = p.Group.Point().Mul(a_vec[n1:][i], g_vec[:n1][i])
-
+		L = p.Group.Point().Mul(aVec[:n1][i], gVec[n1:][i])
+		R = p.Group.Point().Mul(aVec[n1:][i], gVec[:n1][i])
 	}
 	L = p.Group.Point().Add(p.Group.Point().Mul(cl, h), L)
 	R = p.Group.Point().Add(p.Group.Point().Mul(cr, h), R)
@@ -50,14 +50,30 @@ func (p Prover) RecursiveProve(g_vec []kyber.Point, h, P kyber.Point, a_vec, b_v
 
 	// z is the challenge value
 	var z kyber.Scalar
-	z_inv := p.Group.Scalar().Inv(z)
-	// step 5
-	g_vec1 := make([]kyber.Point, n1)
-	a_vec1 := make([]kyber.Scalar, n1)
+	zInv := p.Group.Scalar().Inv(z)
+	// step 5, step 6
+	gVec1 := make([]kyber.Point, n1)
+	aVec1 := make([]kyber.Scalar, n1)
+	bVec1 := make([]kyber.Scalar, n1)
 	for i := 0; i < n1; i++ {
-		g_vec1[i] = p.Group.Point().Add(g_vec[:n1][i]) * g_vec[n_p:][i] * *x
+		left := p.Group.Scalar().Mul(z, bVec[:n1][i])
+		right := p.Group.Scalar().Mul(zInv, bVec[n1:][i])
+		bVec1[i] = p.Group.Scalar().Add(left, right)
+
+		left1 := p.Group.Point().Mul(zInv, gVec[:n1][i])
+		right1 := p.Group.Point().Mul(z, gVec[n1:][i])
+		gVec1[i] = p.Group.Point().Add(left1, right1)
+
+		left = p.Group.Scalar().Mul(z, aVec[:n1][i])
+		right = p.Group.Scalar().Mul(zInv, aVec[n1:][i])
+		aVec1[i] = p.Group.Scalar().Add(left, right)
 	}
 
-	// step 6
+	z2 := p.Group.Scalar().Mul(z, z)
+	z2Inv := p.Group.Scalar().Inv(z2)
+	left := p.Group.Point().Mul(z2, L)
+	right := p.Group.Point().Mul(z2Inv, R)
+	P1 := p.Group.Point().Add(p.Group.Point().Add(P, left), right)
+	p.RecursiveProve(gVec1, h, P1, aVec1, bVec1, n1)
 	return
 }
