0238d9b76d2f947fb217588f41a3f5146178ca04
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-07-27 19:36:38 +0800

    update

--- iipa/prover.go
+++ iipa/prover.go
@@ -13,67 +13,67 @@ func (p Prover) RecursiveProve(gVec []kyber.Point, h, P kyber.Point, aVec, bVec 
 	if aLen != len(bVec) {
 		return
 	}
-	// step 1
+	// step 1 Prover send aVec to Verifier
 	if n == 1 {
 		return
 	}
 
 	var proofStep []kyber.Scalar
 	// step 2
+	// Prover send aVec[n] to Verifier
 	if n%2 == 1 {
-		aNeg := p.Group.Scalar().Neg(aVec[aLen-1])
-		bNeg := p.Group.Scalar().Neg(bVec[aLen-1])
-		tmp1 := p.Group.Point().Mul(aNeg, gVec[gLen-1])
-		tmp2 := p.Group.Point().Mul(p.Group.Scalar().Mul(aVec[aLen-1], bNeg), h)
+		aNeg := p.Scalar().Neg(aVec[aLen-1])
+		bNeg := p.Scalar().Neg(bVec[aLen-1])
+		tmp1 := p.Point().Mul(aNeg, gVec[gLen-1])
+		tmp2 := p.Point().Mul(p.Scalar().Mul(aVec[aLen-1], bNeg), h)
 
-		P = p.Group.Point().Add(p.Group.Point().Add(tmp1, tmp2), P)
+		P = p.Point().Add(p.Point().Add(tmp1, tmp2), P)
 		proofStep = append(proofStep, aNeg, bNeg)
 		n = n - 1
 	}
+	n1 := n / 2
 
 	// step 3
-	n1 := n / 2
-	cl := p.Group.Scalar().Zero()
-	cr := p.Group.Scalar().Zero()
+	cl := p.Scalar().Zero()
+	cr := p.Scalar().Zero()
 	var L, R kyber.Point
 	for i := 0; i < n1; i++ {
-		tmp1 := p.Group.Scalar().Mul(aVec[:n1][i], bVec[n1:][i])
-		cl = p.Group.Scalar().Add(cl, tmp1)
-		tmp2 := p.Group.Scalar().Mul(aVec[n1:][i], bVec[:n1][i])
-		cr = p.Group.Scalar().Add(cr, tmp2)
-		L = p.Group.Point().Mul(aVec[:n1][i], gVec[n1:][i])
-		R = p.Group.Point().Mul(aVec[n1:][i], gVec[:n1][i])
+		tmp1 := p.Scalar().Mul(aVec[:n1][i], bVec[n1:][i])
+		cl = p.Scalar().Add(cl, tmp1)
+		tmp2 := p.Scalar().Mul(aVec[n1:][i], bVec[:n1][i])
+		cr = p.Scalar().Add(cr, tmp2)
+		L = p.Point().Mul(aVec[:n1][i], gVec[n1:][i])
+		R = p.Point().Mul(aVec[n1:][i], gVec[:n1][i])
 	}
-	L = p.Group.Point().Add(p.Group.Point().Mul(cl, h), L)
-	R = p.Group.Point().Add(p.Group.Point().Mul(cr, h), R)
+	L = p.Point().Add(p.Point().Mul(cl, h), L)
+	R = p.Point().Add(p.Point().Mul(cr, h), R)
 	//p.RecursiveProve(n1)
 
 	// z is the challenge value
 	var z kyber.Scalar
-	zInv := p.Group.Scalar().Inv(z)
+	zInv := p.Scalar().Inv(z)
 	// step 5, step 6
 	gVec1 := make([]kyber.Point, n1)
 	aVec1 := make([]kyber.Scalar, n1)
 	bVec1 := make([]kyber.Scalar, n1)
 	for i := 0; i < n1; i++ {
-		left := p.Group.Scalar().Mul(z, bVec[:n1][i])
-		right := p.Group.Scalar().Mul(zInv, bVec[n1:][i])
-		bVec1[i] = p.Group.Scalar().Add(left, right)
+		left := p.Scalar().Mul(z, bVec[:n1][i])
+		right := p.Scalar().Mul(zInv, bVec[n1:][i])
+		bVec1[i] = p.Scalar().Add(left, right)
 
-		left1 := p.Group.Point().Mul(zInv, gVec[:n1][i])
-		right1 := p.Group.Point().Mul(z, gVec[n1:][i])
-		gVec1[i] = p.Group.Point().Add(left1, right1)
+		left1 := p.Point().Mul(zInv, gVec[:n1][i])
+		right1 := p.Point().Mul(z, gVec[n1:][i])
+		gVec1[i] = p.Point().Add(left1, right1)
 
-		left = p.Group.Scalar().Mul(z, aVec[:n1][i])
-		right = p.Group.Scalar().Mul(zInv, aVec[n1:][i])
-		aVec1[i] = p.Group.Scalar().Add(left, right)
+		left = p.Scalar().Mul(z, aVec[:n1][i])
+		right = p.Scalar().Mul(zInv, aVec[n1:][i])
+		aVec1[i] = p.Scalar().Add(left, right)
 	}
 
-	z2 := p.Group.Scalar().Mul(z, z)
-	z2Inv := p.Group.Scalar().Inv(z2)
-	left := p.Group.Point().Mul(z2, L)
-	right := p.Group.Point().Mul(z2Inv, R)
-	P1 := p.Group.Point().Add(p.Group.Point().Add(P, left), right)
+	z2 := p.Scalar().Mul(z, z)
+	z2Inv := p.Scalar().Inv(z2)
+
+	P1 := p.Point().Add(p.Point().Add(P, p.Point().Mul(z2, L)), p.Point().Mul(z2Inv, R))
 	p.RecursiveProve(gVec1, h, P1, aVec1, bVec1, n1)
 	return
 }
--- iipa/verifier.go
+++ iipa/verifier.go
@@ -14,18 +14,53 @@ func NewVerifier() *Verifier {
 }
 
 func (v *Verifier) RecursiveVerify(gVec []kyber.Point, h, P kyber.Point, n int) bool {
+	// step 1
+	// 1.1 Verifier receive aVec from Prover (length of aVec == 1)
+	// 1.2 verify P
 	if n == 1 {
 		var a, b kyber.Scalar
-		left := v.Group.Point().Mul(a, gVec[0])
-		right := v.Group.Point().Mul(v.Group.Scalar().Mul(a, b), h)
-		PWant := v.Group.Point().Add(left, right)
+		left := v.Point().Mul(a, gVec[0])
+		right := v.Point().Mul(v.Scalar().Mul(a, b), h)
+		PWant := v.Point().Add(left, right)
 		return P.Equal(PWant)
 	}
-	if n%2 == 1 {
 
+	// step 2
+	// 2.1 Verifier receive aVec[n], bVec[n] from Prover
+	// 2.3 update P, aVec, gVec, n
+	if n%2 == 1 {
+		var aVecN, bVecN kyber.Scalar
+		aNeg := v.Scalar().Neg(aVecN)
+		bNeg := v.Scalar().Neg(bVecN)
+		tmp1 := v.Point().Mul(aNeg, gVec[n-1])
+		tmp2 := v.Point().Mul(v.Scalar().Mul(aVecN, bNeg), h)
+
+		P = v.Point().Add(v.Point().Add(tmp1, tmp2), P)
+		n = n - 1
 	}
 
-	x := v.Group.Scalar()
-	
-	return false
+	n1 := n / 2
+
+	// step 3
+	// Verifier receive L and R from Prover
+	var L, R = v.Point(), v.Point()
+
+	// step 4
+	// generate challenge value
+	z := v.Scalar()
+	zInv := v.Scalar().Inv(z)
+
+	// step 5
+	gVec1 := make([]kyber.Point, n1)
+	for i := 0; i < n1; i++ {
+		left1 := v.Point().Mul(zInv, gVec[:n1][i])
+		right1 := v.Point().Mul(z, gVec[n1:][i])
+		gVec1[i] = v.Point().Add(left1, right1)
+	}
+	z2 := v.Scalar().Mul(z, z)
+	z2Inv := v.Scalar().Inv(z2)
+	P1 := v.Point().Add(v.Point().Add(P, v.Point().Mul(z2, L)), v.Point().Mul(z2Inv, R))
+
+	ret := v.RecursiveVerify(gVec1, h, P1, n1)
+	return ret
 }
