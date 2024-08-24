47f305b5bb981c25360a2bee80becef428328de7
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-08-25 01:06:16 +0800

    Update timeSharingCompare.go

--- timeSharingCompare.go
+++ timeSharingCompare.go
@@ -4,49 +4,66 @@ import (
 	"crypto/rand"
 	"fmt"
 	"github.com/QinYuuuu/crypto/elgamal"
+	"github.com/QinYuuuu/crypto/utils"
+	"github.com/QinYuuuu/crypto/utils/polynomial"
+	"go.dedis.ch/kyber/v3"
 	"go.dedis.ch/kyber/v3/group/curve25519"
 	"math/big"
 	"time"
 )
-
-// GeneratePrime generates a prime number of the specified bit length.
-func GeneratePrime(bits int) *big.Int {
-	prime, _ := rand.Prime(rand.Reader, bits)
-	return prime
-}
-
-// FindPrimitiveRoot finds a primitive root for a prime p.
-func FindPrimitiveRoot(p *big.Int) *big.Int {
-	if p.Cmp(big.NewInt(2)) == 0 {
-		return big.NewInt(1)
-	}
-	p1 := big.NewInt(2)
-	p2 := new(big.Int).Sub(p, big.NewInt(1))
-	p2.Div(p2, p1)
-
-	for {
-		g, _ := rand.Int(rand.Reader, new(big.Int).Sub(p, big.NewInt(2)))
-		g.Add(g, big.NewInt(2))
-
-		if new(big.Int).Exp(g, new(big.Int).Div(new(big.Int).Sub(p, big.NewInt(1)), p1), p).Cmp(big.NewInt(1)) != 0 &&
-			new(big.Int).Exp(g, new(big.Int).Div(new(big.Int).Sub(p, big.NewInt(1)), p2), p).Cmp(big.NewInt(1)) != 0 {
-			return g
-		}
-	}
-}
-
-// GenerateLargePrimeAndRoot generates a large prime and its primitive root.
-func GenerateLargePrimeAndRoot(bits int) (*big.Int, *big.Int) {
-	p := GeneratePrime(bits)
-	g := FindPrimitiveRoot(p)
-	return p, g
-}
 
 // MeasureTime measures the execution time of a simulation function.
 func MeasureTime(simulateFunc func(B, n int, p, g, pElgamal, gElgamal *big.Int), B, n int, p, g, pElgamal, gElgamal *big.Int) time.Duration {
 	startTime := time.Now()
 	simulateFunc(B, n, p, g, pElgamal, gElgamal)
 	return time.Since(startTime)
+}
+
+func evaluatePolynomials(polys []polynomial.Polynomial, n int, p *big.Int) [][]*big.Int {
+	evals := make([][]*big.Int, len(polys))
+	for k, poly := range polys {
+		evals[k] = make([]*big.Int, n)
+		for i := 0; i < n; i++ {
+			evals[k][i] = poly.EvalMod(new(big.Int).SetInt64(int64(i)), p)
+		}
+	}
+	return evals
+}
+
+func simulate_test1(B, n, t int, g kyber.Point, p *big.Int, suite kyber.Group, pk kyber.Point, sk kyber.Scalar) ([][]*big.Int, kyber.Point, kyber.Point) {
+
+	// Generate B+n random polynomials of degree n
+	polynomials := make([]polynomial.Polynomial, B+n)
+	for i := 0; i < B+n; i++ {
+		polynomials[i], _ = polynomial.NewRand(t, p)
+	}
+
+	// Evaluate polynomials at each point i in [1, n]
+	evals := evaluatePolynomials(polynomials, n, p)
+
+	// Encrypt all evaluations at each point using symmetric enc and Encrypt
+	key := utils.RandomNum(p)
+	K, C, _ := elgamal.Encrypt(suite, pk, key.Bytes())
+	for i := 0; i < B+n; i++ {
+		for j := 0; j < t; j++ {
+		}
+	}
+	encryptedValuesPerPoint := make([][]*big.Int, len(polynomials))
+	for i := 0; i < len(polynomials); i++ {
+		for j := 0; j < n; j++ {
+			encryptedValuesPerPoint[i][j] = new(big.Int).Add(key, evals[i][j])
+		}
+	}
+	return encryptedValuesPerPoint, K, C
+}
+
+func simulateTest2(B, n, t int, p *big.Int) {
+	fs := [generate_random_polynomial(n, p) for _ in range(B)]
+	gs = [generate_random_polynomial(n, p) for _ in range(n)]
+	points = list(range(1, n + 1))
+	evals = evaluate_polynomials(fs, points, p)
+	evals = evaluate_polynomials(gs, points, p)
+	random_linear_combination(fs, gs, points, p)
 }
 
 func main() {
